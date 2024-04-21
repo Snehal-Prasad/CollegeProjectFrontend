4f6e52da34a31ba5991228848d8430fd5afa87c2
Snehal-Prasad <snehalprasad101@gmail.com>
2024-04-21 09:59:43 +0530

    header and background image developed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
+// src/App.js
+
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import StudentSignup from "./components/Student/StudentSignup";
 import StudentLogin from "./components/Student/StudentLogin";
 import TeacherSignup from "./components/Teacher/TeacherSignup";
 import TeacherLogin from "./components/Teacher/TeacherLogin";
-import Welcome from "./components/Welcome/Welcome"; // Import Welcome component
+import Welcome from "./components/Welcome/Welcome";
+import ExamOptionsPage from "./components/Exam/ExamOptionsPage";
+import MCQExamPage from "./components/MCQ/MCQExamPage";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        {/* <Route path="/" element={<Welcome />} /> */}
         <Route path="/student/signup" element={<StudentSignup />} />
         <Route path="/student/login" element={<StudentLogin />} />
         <Route path="/teacher/signup" element={<TeacherSignup />} />
         <Route path="/teacher/login" element={<TeacherLogin />} />
+        <Route path="/exam/options" element={<ExamOptionsPage />} />
+        <Route path="/exams/:examType" element={<MCQExamPage />} />
+        {/* <Route
+          path="/exams/cpp/questions"
+          element={<MCQExamPage examType="cpp" />}
+        />{" "} */}
+        {/* Route for displaying C++ MCQ questions */}
+        <Route path="/welcome" element={<Welcome />} />
       </Routes>
     </Router>
   );
